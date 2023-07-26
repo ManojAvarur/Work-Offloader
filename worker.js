3820e2db173941f410a54ea12ae97aa0d9028246
Manoj <manojavarur@gmail.com>
2023-07-26 19:30:19 +0530

    Issue in Error handler for worker is fixed

--- worker.js
+++ worker.js
@@ -10,12 +10,10 @@ const logFileName = args[2];
 const logWriteLocation = join( logsFolderLocation, logFileName );
 
 const data = { 'Currently Executing Command': command, 'Log File Name': logFileName };
-console.log( data );
-
 
 exec( command, ( error, stdout, stderr ) => {
     if( logFileName !== 'null' ){
-        error = ( error )? `Error : ${error.split('\n').join('\n\t')}` : '';
+        error = ( error?.message )? `\n\nError : ${error.message.split('\n').join('\n\t')}` : '';
         stdout = ( stdout )? `\n\nStdout : ${stdout.split('\n').join('\n\t')}` : '';
         stderr = ( stderr )? `\n\nStderr : ${stderr.split('\n').join('\n\t')}` : '';
 
